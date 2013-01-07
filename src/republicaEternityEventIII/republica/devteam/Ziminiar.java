519157c25b98c7979118e025cbd4a57590200296
felreava <felreava@gmail.com>
2013-01-07 01:30:30 -0500

    Update src/republicaEternityEventIII/republica/devteam/Ziminiar.java

--- src/republicaEternityEventIII/republica/devteam/Ziminiar.java
+++ src/republicaEternityEventIII/republica/devteam/Ziminiar.java
@@ -44,27 +44,56 @@ public class Ziminiar {
 		//TODO: Give Ziminiar his weapons
 	}
 	
-		public boolean ZiminiarArmor(CommandSender sender, Command cmd, String label, String[] args) {
-				// Ziminiar Armor.
-				// Todo: Give acctual armor instead of just diamond.
-				if (cmd.getName().equalsIgnoreCase("ziminiararmor"))
-				{
-					Player player = (Player) sender;
-					PlayerInventory inv = player.getInventory();
-				
-					ItemStack chest = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
-					ItemStack pants = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
-					ItemStack helm = new ItemStack(Material.DIAMOND_HELMET, 1);
-					ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
-					
-					inv.setChestplate(chest);
-					inv.setLeggings(pants);
-					inv.setHelmet(helm);
-					inv.setBoots(boots);
-					return true;
-				}
-				return false;
-	}
+		public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		// Ziminiar's Armor.
+		// Todo: Give acctual armor instead of just diamond.
+		if (cmd.getName().equalsIgnoreCase("armor"))
+		{
+			Player player = (Player) sender;
+			PlayerInventory inv = player.getInventory();
+		
+			ItemStack chest = new ItemStack(Material.DIAMOND_CHESTPLATE, 1);
+			ItemStack pants = new ItemStack(Material.DIAMOND_LEGGINGS, 1);
+			ItemStack helm = new ItemStack(Material.DIAMOND_HELMET, 1);
+			ItemStack boots = new ItemStack(Material.DIAMOND_BOOTS, 1);
+			
+			//Boot Enchantments, NBT data to be added. Don't worry Prod, I'll do it for you.
+			boots.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 6);
+			boots.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 2);
+			boots.addUnsafeEnchantment(Enchantment.PROTECTION_FALL, 5);
+			boots.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+			boots.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 6);
+			
+			
+			//Helmet Enchantments, same about the NBT.
+			helm.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 6);
+			helm.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 3);
+			helm.addEnchantment(Enchantment.PROTECTION_FIRE, 3);
+			
+			//Chestplate Enchantments, NBT yada yada.
+			
+			chest.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 7);
+			chest.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE, 6);
+			chest.addUnsafeEnchantment(Enchantment.PROTECTION_FIRE, 5);
+			chest.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+			
+			//Leggings Enchantments, NBT? What are you talking about? Why would I mention that again?
+			
+			pants.addUnsafeEnchantment(Enchantment.PROTECTION_ENVIRONMENTAL, 6);
+			pants.addEnchantment(Enchantment.PROTECTION_EXPLOSIONS, 4);
+			pants.addEnchantment(Enchantment.PROTECTION_FIRE, 4);
+			pants.addUnsafeEnchantment(Enchantment.PROTECTION_PROJECTILE,5);
+			
+			inv.setChestplate(chest);
+			inv.setLeggings(pants);
+			inv.setHelmet(helm);
+			inv.setBoots(boots);
+			
+			return true;
+		}
+		return false;
+}
+
 	
 	private void damageZiminiarHealth(int i){
 		ZiminiarHealth -= i;
