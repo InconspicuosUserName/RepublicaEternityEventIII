2c64cb91df80f66bb0eccaf30f7e49a22fe86257
Kyle Nehman <felreava@gmail.com>
2013-02-26 21:42:09 -0500

    Update
    Prod, fix my shit

--- src/republicaEternityEventIII/republica/devteam/EternityListener.java
+++ src/republicaEternityEventIII/republica/devteam/EternityListener.java
@@ -1,18 +1,58 @@
 package republicaEternityEventIII.republica.devteam;
 
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
 
-public class EternityListener implements Listener{
+import com.avaje.ebean.EbeanServer;
+import republicaEternityEventIII.republica.devteam.FireworkDelayedBurster;
+
+public class EternityListener implements Listener, Plugin{
 	
 	private EternityMain em;
 	public EternityListener(EternityMain em) {
 		super();
 		this.em = em;
 	}
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e)
+	{
+		Player p = (Player) e.getEntity();
+		 if (p.hasPermission("ziminiar.level")){
+			 p.sendMessage(ChatColor.RED + "Test");
+		    	getServer().getScheduler().scheduleSyncRepeatingTask(this, new FireworkDelayedBurster(p), 45L, 45L);    	
+		    	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
+		               public void run()
+		               {
+		                   Bukkit.getScheduler().cancelAllTasks();
+		               }
+		           }, 2400L);
+		    }
+			 
+		            
+			 
+		 
+		 }
+	
 	
 	@EventHandler
 	public void onPlayerDamageMethod(EntityDamageByEntityEvent e){
@@ -36,6 +76,140 @@ public class EternityListener implements Listener{
 		if (SignPunchingOMatic.changed()) {
 			em.saveResultsSignLocation();
 		}
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender arg0, Command arg1,
+			String arg2, String[] arg3) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
+			String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public FileConfiguration getConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public File getDataFolder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public EbeanServer getDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ChunkGenerator getDefaultWorldGenerator(String arg0, String arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PluginDescriptionFile getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Logger getLogger() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PluginLoader getPluginLoader() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public InputStream getResource(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Server getServer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isNaggable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onLoad() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reloadConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveDefaultConfig() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveResource(String arg0, boolean arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNaggable(boolean arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
---
+++ src/republicaEternityEventIII/republica/devteam/FireworkDelayedBurster.java
@@ -0,0 +1,72 @@
+package republicaEternityEventIII.republica.devteam;
+
+import java.util.Random;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+public class FireworkDelayedBurster implements Runnable {
+	
+	private Player p;
+
+	public FireworkDelayedBurster(Player pInit){
+	    p = pInit;
+}
+	  public void run() {
+	        // firework spawning stuff goes here instead of onDeath
+		  for(int i = 0; i < 5; i++){	
+			 
+			  
+	            //Spawn the Firework
+	            Firework fw = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
+	            FireworkMeta fwm = fw.getFireworkMeta();
+
+	            //random
+	            Random ra = new Random();
+
+	            //sets type
+	            int rt = ra.nextInt(5) + 1;
+	            Type type = Type.BALL;
+	            if (rt == 1) type = Type.BALL;
+	            if (rt == 2) type = Type.BALL_LARGE;
+	            if (rt == 3) type = Type.BURST;
+	            if (rt == 4) type = Type.CREEPER;
+	            if (rt == 5) type = Type.STAR;
+
+	            //colors
+	            //To be Added
+	            
+	            
+	            
+	            int r = ra.nextInt(256);
+	            int b = ra.nextInt(256);
+	            int g = ra.nextInt(256);
+	            Color c1 = Color.fromRGB(r, g, b);
+
+	            r = ra.nextInt(256);
+	            b = ra.nextInt(256);
+	            g = ra.nextInt(256);
+	            Color c2 = Color.fromRGB(r, g, b);
+
+
+	            //effect
+	            FireworkEffect effect = FireworkEffect.builder().flicker(ra.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(ra.nextBoolean()).build();
+
+	            //applied effects
+	            fwm.addEffect(effect);
+
+	            //random power! moar sulphur!
+	            int rp = ra.nextInt(2) + 1;
+	            fwm.setPower(rp);
+
+	            //aaaaaand set it
+	            fw.setFireworkMeta(fwm);
+	  }
+	}
+}
+
