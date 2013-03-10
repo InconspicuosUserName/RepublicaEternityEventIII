a38067995ac10f3b4f998c9437b34278521ba10a
Kyle Nehman <felreava@gmail.com>
2013-03-10 16:13:05 -0400

    Fireworks Fix
    Everything Fireworks now works!

--- src/republicaEternityEventIII/republica/devteam/EternityListener.java
+++ src/republicaEternityEventIII/republica/devteam/EternityListener.java
@@ -3,14 +3,20 @@ package republicaEternityEventIII.republica.devteam;
 import java.io.File;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Random;
 import java.util.logging.Logger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
 import org.bukkit.Server;
+import org.bukkit.FireworkEffect.Type;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,6 +24,7 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.inventory.meta.FireworkMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginLoader;
@@ -38,14 +45,63 @@ public class EternityListener implements Listener, Plugin{
 	{
 		Player p = (Player) e.getEntity();
 		 if (p.hasPermission("ziminiar.level")){
-			 p.sendMessage(ChatColor.RED + "Test");
-		    	getServer().getScheduler().scheduleSyncRepeatingTask(this, new FireworkDelayedBurster(p), 45L, 45L);    	
-		    	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable(){
-		               public void run()
-		               {
-		                   Bukkit.getScheduler().cancelAllTasks();
-		               }
-		           }, 2400L);
+			 //Sorry for this spam, but I'm not risking shit being fucked up now!
+			 for(int i = 0; i < 200; i++){	
+				 //Spawn the Firework
+		            Firework fw = (Firework) p.getWorld().spawnEntity(p.getLocation(), EntityType.FIREWORK);
+		            FireworkMeta fwm = fw.getFireworkMeta();
+
+		            //random
+		            Random ra = new Random();
+
+		            //sets type
+		            int rt = ra.nextInt(5) + 1;
+		            Type type = Type.BALL;
+		            if (rt == 1) type = Type.BALL;
+		            if (rt == 2) type = Type.BALL_LARGE;
+		            if (rt == 3) type = Type.BURST;
+		            if (rt == 4) type = Type.CREEPER;
+		            if (rt == 5) type = Type.STAR;
+
+		            //colors
+		            //To be Added
+		            
+		            
+		            
+		            int r = ra.nextInt(256);
+		            int b = ra.nextInt(256);
+		            int g = ra.nextInt(256);
+		            Color c1 = Color.fromRGB(r, g, b);
+
+		            r = ra.nextInt(256);
+		            b = ra.nextInt(256);
+		            g = ra.nextInt(256);
+		            Color c2 = Color.fromRGB(r, g, b);
+
+
+		            //effect
+		            FireworkEffect effect = FireworkEffect.builder().flicker(ra.nextBoolean()).withColor(c1).withFade(c2).with(type).trail(ra.nextBoolean()).build();
+
+		            //applied effects
+		            fwm.addEffect(effect);
+
+		            //random power! moar sulphur!
+		            int rp = ra.nextInt(2) + 1;
+		            fwm.setPower(rp);
+
+		            //aaaaaand set it
+		            fw.setFireworkMeta(fwm);
+			 }
+		    	p.sendMessage(ChatColor.RED + "Do not release your spirit!");
+		    	p.sendMessage(ChatColor.RED + "Seriously though, stay still for two minutes while the fireworks go off.");
+		    	
+			    	Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask((Plugin) this, new FireworkDelayedBurster(p), 45L, 45L);    	
+			    	
+			    	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, new Runnable(){
+			               public void run(){
+			                   Bukkit.getScheduler().cancelAllTasks();
+			               }
+			           }, 2400L);
 		    }
 			 
 		            
